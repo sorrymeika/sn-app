61b9cc4d5da1f3ab0e82ecf976c7f3a84fed9a4d
孙路 <>
2019-11-26 11:35:19 +0800

    优化SfsImage

--- src/components/SfsImage.jsx
+++ src/components/SfsImage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { inject } from "snowball/app";
 
-function SfsImage({ completeUrl, src, size, quantity, type, ...props }) {
+/**
+ * sonofs图片
+ * @param {*} props
+ * @param {*} props.type 类型： enum { 0: '仅原格式为jpg才压缩', 1: '导出jpeg', 2: '支持webp的导出webp，否则导出jpg' }
+ */
+function SfsImage({ completeUrl, src, size, quantity, type = 0, ...props }) {
     if (src) {
         props.src = completeUrl(src, size, [quantity || 80, type].filter(opt => !!opt).join('-'));
     }
